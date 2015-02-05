c64010b54dc1134b071a052ad98a008cdee215a1
my <nakanotadaichiro@gmail.com>
2015-02-05 16:04:42 +0900

    fix

--- lib/embulk/input_fileinfo.rb
+++ lib/embulk/input_fileinfo.rb
@@ -1,62 +1,62 @@
 
 module Embulk
 
-	class InputFileInformation < InputPlugin
-		require 'find'
+  class InputFileInformation < InputPlugin
+    require 'find'
 
-		Plugin.register_input('fileinfo', self)
+    Plugin.register_input('fileinfo', self)
 
-		def self.transaction(config, &control)
-			
-			threads = 1
-			rootdir = config.param('rootdir', :string)
-			ext = config.param('extension', :string, default: '*')
-			sizelowerlimit = config.param('sizelowerlimit', :long, default: 0)
+    def self.transaction(config, &control)
+      
+      threads = 1
+      rootdir = config.param('rootdir', :string)
+      ext = config.param('extension', :string, default: '*')
+      sizelowerlimit = config.param('sizelowerlimit', :long, default: 0)
 
-			task = {'rootdir' => rootdir, 'ext' => ext, 'sizelowerlimit' => sizelowerlimit}
+      task = {'rootdir' => rootdir, 'ext' => ext, 'sizelowerlimit' => sizelowerlimit}
 
-			columns = [
-				Column.new(0, 'FILE_NAME', :string),
-				Column.new(1, 'FILE_PATH', :string),
-				Column.new(2, 'FILE_SIZE_RAW', :long)
-			]
+      columns = [
+        Column.new(0, 'FILE_NAME', :string),
+        Column.new(1, 'FILE_PATH', :string),
+        Column.new(2, 'FILE_SIZE_RAW', :long)
+      ]
 
-			puts "File information generation started."
-			commit_reports = yield(task, columns, threads)
-			puts "File information input finished."
+      puts "File information generation started."
+      commit_reports = yield(task, columns, threads)
+      puts "File information input finished."
 
-			return {}
+      return {}
 
-		end
+    end
 
-		def initialize(task, schema, index, page_builder)
-			super
-		end
+    def initialize(task, schema, index, page_builder)
+      super
+    end
 
-		def run
+    def run
 
-			rootdir = @task['rootdir']
-			ext = @task['ext']
-			sizelowerlimit = @task['sizelowerlimit']
+      rootdir = @task['rootdir']
+      ext = @task['ext']
+      sizelowerlimit = @task['sizelowerlimit']
 
-			Dir.glob(rootdir + '/**/*') {|f|
+      Dir.glob(rootdir + '/**/*') {|f|
 
-			  next unless FileTest.file?(f)
-			  
-			  s = File::stat(f)
+        next unless FileTest.file?(f)
+        
+        s = File::stat(f)
 
-			  next if ext != "*" && File::extname(f) == ext 
-			  next if s.size < sizelowerlimit * 1024 * 1024
+        next if ext != "*" && File::extname(f) == ext 
+        next if s.size < sizelowerlimit * 1024 * 1024
 
-			  @page_builder.add([File::basename(f), File::dirname(f), s.size])
+        @page_builder.add([File::basename(f), File::dirname(f), s.size])
 
-			}
+      }
 
-			@page_builder.finish
+      @page_builder.finish
 
-      		commit_report = {
-      		}
-			return commit_report
-		end
-	end
+          commit_report = {
+          }
+      return commit_report
+    end
+  end
 end
